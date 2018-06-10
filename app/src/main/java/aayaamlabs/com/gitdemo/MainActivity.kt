3834f1f1c2ecfceb8c052732299ec932807ea00a
al-delta <al-delta@aldelta-To-be-filled-by-O-E-M>
2018-06-10 18:00:07 +0530

    initial comment

--- app/src/main/java/aayaamlabs/com/gitdemo/MainActivity.kt
+++ app/src/main/java/aayaamlabs/com/gitdemo/MainActivity.kt
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+init {
 
+}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
